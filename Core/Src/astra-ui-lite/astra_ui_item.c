06348a53b726f18e81c11a489f74381b4b009fd1
AstraThreshold <fir@live.it>
2024-12-23 23:52:46 +0800

    wtf?

--- Core/Src/astra-ui-lite/astra_ui_item.c
+++ Core/Src/astra-ui-lite/astra_ui_item.c
@@ -8,4 +8,5 @@ void astra_draw_pop_window(uint8_t _x, uint8_t _y, uint8_t _width, uint8_t _heig
   oled_set_font(u8g2_font_wqy12_t_chinese1);
   oled_set_draw_color(0); //黑遮罩打底
   //todo
+  //todo
 }
